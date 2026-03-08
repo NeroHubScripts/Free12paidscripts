14ced9d85535c44c4266c1398e2ef175c7773613
NeroHubScripts <musaalukaaa72@gmail.com>
2026-03-08 23:34:48 +0100

    Update 12u1.lua

--- 12u1.lua
+++ 12u1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://pastebin.com/raw/J7w4gNDA'))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/7eed04278e8a444cad53176fa7179daf0cc812a88a97da867a280577c2fe06f0/download"))()
